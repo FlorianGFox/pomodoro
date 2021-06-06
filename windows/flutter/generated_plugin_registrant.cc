ebccb96681bc972105f55d4926fbe761566f5648
Florian Grundmann <florian.grundmann@freenet.de>
2021-06-06 16:36:00 +0700

    Created Windows files.

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <desktop_window/desktop_window_plugin.h>
+#include <menubar/menubar_plugin.h>
+#include <window_size/window_size_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DesktopWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopWindowPlugin"));
+  MenubarPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MenubarPlugin"));
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
+}
